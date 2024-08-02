008ed44082dfa3ef62e8d1c57994468f60824253
mandanya <dan.zholk@gmail.com>
2024-08-02 20:22:48 +0300

    gemma

--- src/core.py
+++ src/core.py
@@ -7,7 +7,7 @@ import math
 from io import BytesIO
 from datetime import datetime
 
-from prompts import scale, prompt_rerank, cv_scale, base_prompt
+from prompts import scale, prompt_rerank, cv_scale, base_prompt, format_example
 
 
 load_dotenv()
@@ -44,8 +44,33 @@ def parse_json_scale(txt: str, return_full=False) -> dict | list[int]:
                 return [int(v) for _, v in parsed.items()]
             except:
                 raise ValueError("Invalid json format")
+    elif splitted[0] == '```json' and splitted[-1] == '' and splitted[-2].strip() == '```':
+        # finish_index = len(splitted) - 1
+        # max_steps = 5
+        # step = 0
+        #
+        # while step <= max_steps and splitted[finish_index - step].strip() != '```':
+        #     print(step, splitted[finish_index - step])
+        #     step += 1
+        #
+        # new_finish_index = finish_index - step - 1
+        #
+        # if splitted[new_finish_index] != '```':
+        #     raise ValueError(f"Invalid json format, step={step} {txt}")
+
+        # parsed = json.loads('\n'.join(splitted[1:new_finish_index]))
+
+        parsed = json.loads('\n'.join(splitted[1:-2]))
+
+        if return_full:
+            return {k: int(v) for k, v in parsed.items()}
+        else:
+            try:
+                return [int(v) if isinstance(v, int) else 3 for _, v in parsed.items()]
+            except:
+                raise ValueError(f"Invalid json format {parsed}")
     else:
-        raise ValueError("Invalid json format")
+        raise ValueError(f"Invalid json format {txt}")
 
 
 def match_score(meme, resume) -> float:
@@ -60,20 +85,32 @@ class CVMEME:
             scale_prompt: str = scale,
             rerank_prompt: str = prompt_rerank,
             base_prompt: str = base_prompt,
+            format_example_prompt: str = format_example,
             cv_scale_prompt: str = cv_scale,
             memes_base_dir: str = '../data/meme',
-            log_path: str = '../data/log.txt'
+            log_path: str = '../data/log.txt',
+            # model_name: str = "gpt-4o-mini"
+            model_name: str = "meta-llama/Meta-Llama-3.1-8B-Instruct"
     ):
         self.scale_prompt = scale_prompt
         self.rerank_prompt = rerank_prompt
         self.cv_scale_prompt = cv_scale_prompt
         self.base_prompt = base_prompt
+        self.format_example_prompt = format_example_prompt
+
         self.log_path = log_path
 
         self.memes_base_dir = memes_base_dir
 
+        self.model = model_name
+
+        # self.client = OpenAI(
+        #     api_key=os.environ['OPENAI_API_KEY'],
+        # )
+
         self.client = OpenAI(
             api_key=os.environ['OPENAI_API_KEY'],
+            base_url=os.environ['LLM_HOST']
         )
 
         with open(scores_path, "r", encoding="utf-8") as json_file:
@@ -90,30 +127,30 @@ class CVMEME:
                 log_file.write(f"User ID: {user_id} => Got question ({interaction_time})\n\n")
 
         response = self.client.chat.completions.create(
-            model="gpt-4o-mini",
+            model=self.model,
             messages=[
-                {
-                    "role": "system",
-                    "content": [
-                        {"type": "text", "text": self.base_prompt},
-                    ],
-                },
+                # {
+                #     "role": "system",
+                #     "content": [
+                #         {"type": "text", "text": self.base_prompt},
+                #     ],
+                # },
                 {
                     "role": "user",
                     "content": [
-                        {"type": "text", "text": txt},
+                        {"type": "text", "text": self.base_prompt+'\n\nСообщение:\n'+txt},
                     ],
                 }
             ],
             max_tokens=512,
-            temperature=1,
+            temperature=0.3,
         )
 
         return response.choices[0].message.content
 
     def get_cv_scale(self, txt: str) -> str:
         response = self.client.chat.completions.create(
-            model="gpt-4o-mini",
+            model=self.model,
             messages=[
                 {
                     "role": "user",
@@ -121,7 +158,7 @@ class CVMEME:
                         {"type": "text", "text": self.cv_scale_prompt.format(
                             cv=txt,
                             scale=self.scale_prompt
-                        )},
+                        ) + self.format_example_prompt},
                     ],
                 }
             ],
@@ -135,7 +172,7 @@ class CVMEME:
         meme1, meme2, meme3 = memes
 
         response = self.client.chat.completions.create(
-            model="gpt-4o-mini",
+            model=self.model,
             messages=[
                 {
                     "role": "user",
@@ -156,13 +193,13 @@ class CVMEME:
                 }
             ],
             max_tokens=128,
-            temperature=0.75,
+            temperature=0.3,
         )
 
         return response.choices[0].message.content
 
-    def process_cv(self, file: str | BytesIO) -> tuple[str, str]:
-        text = extract_text_from_pdf(file)
+    def process_cv(self, file: str | BytesIO, bytes=True) -> tuple[str, str]:
+        text = extract_text_from_pdf(file, bytes=bytes)
         return text, self.get_cv_scale(text)
 
     def get_scores(self, cv_scale: str, return_scores: bool = False):
@@ -224,7 +261,7 @@ class CVMEME:
                 interaction_time = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
                 log_file.write(f"User ID: {user_id} => Got Scores ({cv_scores}) ({interaction_time})\n\n")
 
-        meme = self.rerank_memes(cv, cv_scores)
+        meme = self.rerank_memes(cv, cv_scores).split('\n')[0].strip()
 
         if log:
             with open(self.log_path, "a") as log_file:
